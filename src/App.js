4412d9db789ef7d998cd9741696e7efd51790f87
mromualdo77 <mromualdo77@gmail.com>
2025-08-24 17:08:45 -0300

    incluindo o led da dio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import { View, Image,Text, StyleSheet, SafeAreaView, StatusBar, Pressable, Linking, } from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Pressable,
+  Linking,
+} from 'react-native';
 
 const colorGithub = '#010409';
 const colorFontGithub = '#C9D1D9';
 const colorDarkFontGithub = '#4F565E';
+const matrixDio = [
+  [0, 0, 0, 0,    0, 0, 0, 0, 0,    0, 0, 0, 0,],
+  [0, 1, 1, 0,    0, 0, 1, 0, 0,    0, 1, 0, 0,],
+  [0, 1, 0, 1,    0, 0, 1, 0, 0,    1, 0, 1, 0,],
+  [0, 1, 0, 1,    0, 0, 1, 0, 0,    1, 0, 1, 0,],
+  [0, 1, 1, 0,    0, 0, 1, 0, 0,    0, 1, 0, 0,],
+  [0, 0, 0, 0,    0, 0, 0, 0, 0,    0, 0, 0, 0, ],
+];
 
 const imageProfileGithub =
   'https://avatars.githubusercontent.com/u/90730177?v=4';
-  const urlToMyGithub ='https://github.com/mromualdo77';
+const urlToMyGithub = 'https://github.com/mromualdo77';
 
 const App = () => {
-    const handlePressGoToGithub = async () => {
-        console.log('Verificando link');
-        const res = await Linking.canOpenURL(urlToMyGithub);
-        if(res) {
-            console.log('Link aprovado');
-            console.log('Abrindo link.....');
-            await Linking.openURL(urlToMyGithub);
-        }
-    };
+  const handlePressGoToGithub = async () => {
+    console.log('Verificando link');
+    const res = await Linking.canOpenURL(urlToMyGithub);
+    if (res) {
+      console.log('Link aprovado');
+      console.log('Abrindo link.....');
+      await Linking.openURL(urlToMyGithub);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -30,32 +47,46 @@ const App = () => {
           style={styles.avatar}
           source={{ uri: imageProfileGithub }}
         />
-            <Text
-            accessibilityLabel="Nome: maria romualdo"
-            style={[styles.defaultText, styles.name]}
-            >Maria Romualdo                
-            </Text>
+        <Text
+          accessibilityLabel="Nome: maria romualdo"
+          style={[styles.defaultText, styles.name]}
+        >
+          Maria Romualdo
+        </Text>
 
-            <Text
-            accessibilityLabel="Nickname: mromualdo77"
-          style={[styles.defaultText, styles.nickname]}>
+        <Text
+          accessibilityLabel="Nickname: mromualdo77"
+          style={[styles.defaultText, styles.nickname]}
+        >
           mromualdo77
-          </Text>
+        </Text>
 
-             <Text
-             accessibilityLabel='Estudante de Front-end Developer'
-             style={[styles.defaultText, styles.description]} >
-                Estudante Front-end Developer
-                "</Text>
+        <Text
+          accessibilityLabel="Estudante de Front-end Developer"
+          style={[styles.defaultText, styles.description]}
+        >
+          Estudante Front-end Developer
+        </Text>
 
-             <Pressable onPress={handlePressGoToGithub}>
-             <View style={styles.button}>
-                <Text style={[styles.defaultText, styles.textButton]}>
-                        Open in Github
-                    </Text>
-            
-                </View>          
-                 </Pressable>
+        <View style={styles.quadradinhosLed}>
+          {matrixDio.map((row, rowIndex) => (
+            <View key={rowIndex} style={styles.row}>
+              {row.map((cell, colIndex) => (
+                <View
+                  key={colIndex}
+                  style={[styles.pixel, cell ? styles.pixelOn : styles.pixelOff]}
+                />
+              ))}
+            </View>
+          ))}
+        </View>
+        <Pressable onPress={handlePressGoToGithub}>
+          <View style={styles.button}>
+            <Text style={[styles.defaultText, styles.textButton]}>
+              Open in Github
+            </Text>
+          </View>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
@@ -68,17 +99,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: colorGithub,
-   // paddingTop: 30,
-   // paddingInlineStart: 10,
+    // paddingTop: 30,
+    // paddingInlineStart: 10,
   },
   /*text: {
         fontSize:20,
         color:'#fff',
     },*/
-    content: {
-        alignItems: 'center',
-        padding:20,
-    },
+  content: {
+    alignItems: 'center',
+    padding: 20,
+  },
 
   avatar: {
     borderRadius: 100,
@@ -87,31 +118,55 @@ const styles = StyleSheet.create({
     borderColor: '#FF5E9A',
     borderWidth: 2,
   },
-  defaultText:{
+  defaultText: {
     color: colorFontGithub,
   },
   name: {
     marginTop: 20,
-    fontWeight:'bold',
-    fontSize:24,
+    fontWeight: 'bold',
+    fontSize: 24,
   },
   nickname: {
     fontSize: 18,
-    color:colorDarkFontGithub,
+    color: colorDarkFontGithub,
   },
   description: {
     fontWeight: 'bold',
-    fontSize:14,
+    fontSize: 14,
   },
+
+  quadradinhosLed: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 20,
+    width: 185,
+    height: 100,
+  },
+
+  pixel: {
+    width: 13,
+    height: 13,
+    margin: 1.5,
+    borderRadius: 5,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  pixelOn: {
+    backgroundColor: '#7cfc00',
+  },
+  pixelOff: {
+    backgroundColor: '#006400',
+  },
+
   button: {
-    marginTop:20,
+    marginTop: 20,
     backgroundColor: colorDarkFontGithub,
-    borderRadius:10,
-    padding:20,
+    borderRadius: 10,
+    padding: 20,
   },
   textButton: {
     fontWeight: 'bold',
     fontSize: 16,
   },
 });
-
